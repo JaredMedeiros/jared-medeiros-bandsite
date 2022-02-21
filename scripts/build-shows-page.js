f7a1add08ac5a2fe5b481906127f60747932eb7b
Jared Medeiros <jared14jared@gmail.com>
2022-02-21 15:17:41 -0500

    clean up script files and added detailed pseudocode

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,56 +1,24 @@
-//Create an array of objects to serve as the shows list content
-// const showsArray = [
-//     {
-//         date: "Mon Sept 06 2021",
-//         venue: "Ronald Lane",
-//         location: "San Francisco, CA"
-//     },
-//     {
-//         date: "Tue Sept 21 2021",
-//         venue: "Pier 3 East",
-//         location: "San Francisco, CA"
-//     },
-//     {
-//         date: "Fri Oct 15 2021",
-//         venue: "View Lounge",
-//         location: "San Francisco, CA"
-//     },
-//     {
-//         date: "Sat Nov 06 2021",
-//         venue: "Hyatt Agency",
-//         location: "San Francisco, CA"
-//     },
-//     {
-//         date: "Fri Nov 26 2021 ",
-//         venue: "Moscow Center",
-//         location: "San Francisco, CA"
-//     },
-//     {
-//         date: "Wed Dec 15 2021",
-//         venue: "Press Club",
-//         location: "San Francisco, CA"
-//     }
-// ]
-
-
-
-
 const apiKey = "a2c4d-a959-4ddc-99a7350-0bbe641068c8";
 const showsUrl = "https://project-1-api.herokuapp.com/showdates?api_key=" + apiKey;
 
 function getShowDates() {
+    //Make an axios call to retrieve the show dates data
     axios
     .get (showsUrl)
     .then(response => {
         console.log(response);
+        //create a shows array with the data from this response
         let showsArray = response.data;
         showsArray.forEach(showArr => {
             displayShowDates (showArr)
         })
+        //create a function that toggles the color of the selected list item
         function toggleColor(show) {
             for (let i = 0; i < show.length; i++) {
               show[i].addEventListener("click", function (e) {
                 for (let i = 0; i < show.length; i++) {
+                    //if a show has not been clicked, remove the active class
+                    //additionally, when a show has been clicked, remove the active class from the previous show
                   if (this !== show[i]) {
                     show[i].classList.remove("shows__card-active");
                   } else if (
@@ -58,6 +26,7 @@ function getShowDates() {
                   ) {
                     this.classList.remove("shows__card-active");
                   } else {
+                      //if a show has been clicked, add the active class
                     this.classList.add("shows__card-active");
                   }
                 }
@@ -65,16 +34,17 @@ function getShowDates() {
               });
             }
           }
+          //call this toggle function
           toggleColor(document.querySelectorAll(".shows__card"));
     })
     .catch((error) => {
         console.log(error);
     })
 }
-
+//Store the shows container as a variable
 let showsContainer = document.querySelector(".shows__card-container");
 
-//Create a function that will display the shows as content within the list items
+//Create a function that will display the shows list items within the unordered list
 function displayShowDates (showArr) {
 
     let show = document.createElement("li");
@@ -85,11 +55,14 @@ function displayShowDates (showArr) {
     dateHeading.classList.add("shows__subtitle");
 
     let showDate = document.createElement("p");
+    //create an array of months and days
     let days = ['Sun','Mon','Tue','Wed','Thurs','Fri','Sat'];
     let months = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec'];
     showDate.innerText = 
+    //convert convert the returned data into a "day" string using the previously made array
     days[new Date(parseInt(showArr.date)).getDay()] +
     " "+
+    //convert convert the returned data into a "day" string using the previously made array
     months[new Date(parseInt(showArr.date)).getMonth()] +
     " "+
     new Date(parseInt(showArr.date)).getDate() +
@@ -128,6 +101,7 @@ function displayShowDates (showArr) {
     show.appendChild(button);
 }
 
+//call this function
 getShowDates();
 
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,32 +1,19 @@
 const apiKey = "a73a2c4d-a959-4ddc-9950-0bbe641068c8";
 const commentUrl = "https://project-1-api.herokuapp.com/comments?api_key=" + apiKey;
 
-
-// axios
-//     .get(commentUrl)
-//     .then(response => {
-//         console.log(response);
-//         let commentArray = response.data;
-//         commentArray.forEach(comm => {
-//             displayComments(comm)
-//         })
-//     })    
-//     .catch(error => {
-//         console.log(error);
-//     })
-
+//Store the comment container as a variable
 let commentContainer = document.querySelector('.comment');
-
+//Create a functino that calls the api
 function getComments() {
     axios  
     .get(commentUrl)
     .then((response) => {
-        // let commentArray = 
+        //Sort the returned data in retured chornological order. Newest items will populate at the top of the list
         response.data.sort((a,b) => {
             return b.timestamp - a.timestamp;
         });
+        //Resest this div
         commentContainer.innerText = "";
-        // console.log(commentArray);
         response.data.forEach((comment) => {
             displayComments(comment);
         });
@@ -35,7 +22,7 @@ function getComments() {
         console.log(error);
     });
     }
-
+//Create a function that displays the comments
 function displayComments (comm) {
     let comment = document.createElement("li");
     comment.classList.add("comment__post");
@@ -66,7 +53,7 @@ function displayComments (comm) {
     commentCopy.innerText = comm.comment;
     commentCopy.classList.add("comment__copy");
         
-            //Append the elements within eachother as structured in the HTML DOC
+    //Append the elements within eachother as structured in the HTML DOC
     commentContainer.appendChild(comment);
     comment.appendChild(commentPicture);
     comment.appendChild(commentProfile);
@@ -75,13 +62,13 @@ function displayComments (comm) {
     commentUser.appendChild(commentDate);
     commentProfile.appendChild(commentCopy);
 }
-
+//Call the get comments function 
 getComments();
 
- 
+//Store the submission form as a variable 
 const submissionForm = document.querySelector(".conversation__form");
 
-
+//Create a function that stores the submitted comment into the api as an object
 function postComment(commenter, comment) {
     let newComment = {
         name: commenter,
@@ -96,21 +83,16 @@ function postComment(commenter, comment) {
             console.log(error);
         });
 }
-
+//When the comment is submitted display it on top of the comment list
 submissionForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     let commName = event.target.commentName.value
     let commCopy = event.target.commentCopy.value
-            
-
-    // let nameField = document.getElementById("conversation--name");
-    // let commentField = document.getElementById("conversation__copy");
-
+    //reset the inputs in the submission form
     if (commName && commCopy) {
         postComment(commName, commCopy)
         submissionForm.reset();
     }
-
-        });
+ });
 
